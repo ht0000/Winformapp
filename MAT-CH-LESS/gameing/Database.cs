0c3f1e1daae2fc8381f7997b209e8ba78fcf3bfb
SAZZAD HOSSAIN TASNIM <66819027+SAZZAD-HT@users.noreply.github.com>
2023-02-17 22:10:39 +0600

    up.cs
    new

--- MAT-CH-LESS/gameing/Database.cs
+++ MAT-CH-LESS/gameing/Database.cs
@@ -9,6 +9,7 @@ namespace gameing
 {
     public class Database
     {
+     //Commenting is Important
         public static SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\SAZZAD\source\repos\labfinalll\db\ht.mdf;Integrated Security=True;Connect Timeout=30");
     }
    
